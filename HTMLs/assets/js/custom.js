ecf7f7e577d72a4d870bc06c3e655534abc08ccb
Kamal Kishore <kamal@mds.asia>
2021-11-14 02:31:40 +0530

    completed landing page except the popover modal

--- HTMLs/assets/js/custom.js
+++ HTMLs/assets/js/custom.js
@@ -1,5 +1,6 @@
 $('.fade').slick({
   dots: true,
+  arrows: false,
   infinite: true,
   speed: 500,
   fade: true,
@@ -18,16 +19,31 @@ $('.storyBox__slider-wrapper').slick({
   autoplaySpeed: 2000,
 });
 
-$('.newsBox__slider').slick({
+$('.js-newsBox__slider').slick({
   dots: false,
   arrows: true,
-  infinite: false,
+  infinite: true,
   autoplay: true,
   slidesToShow: 4,
   autoplaySpeed: 2000,
   variableWidth: true,
   prevArrow:"<button type='button' class='slider-btn slider-btn--prev'><i class='icon-arrow-left slider-btn__icon'></i></button>",
   nextArrow:"<button type='button' class='slider-btn slider-btn--next'><i class='icon-arrow-right slider-btn__icon'></i></button>"
+});
+
+$('.menuBox__button').on('click', function(){
+  $('.menuBox__menu').toggleClass('active');
+  $(this).toggleClass('active');
+});
+
+$('.meetBox__tabs > li > a').click(function(){
+  var t = $(this).attr('id');
+  if($(this).hasClass('inactive')){ //this is the start of our condition 
+    $('.meetBox__tabs > li > a').addClass('inactive');
+    $(this).removeClass('inactive');
+    $('.meetBox__team').removeClass('active');
+    $('#'+ t + 'C').addClass('active');
+ }
 });
 
 $(window).scroll(function() {    
